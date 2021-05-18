df0e6c0096c0668a9689c2d1721c07762eb74526
moocow <efriedla20852@gmail.com>
2021-05-18 15:13:13 -0400

    Fixed search, so you are able to change pokemon by typing a number into the search

--- src/index.js
+++ src/index.js
@@ -39,9 +39,10 @@ function getPokemon(){
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
         document.getElementById("poke1").innerHTML = myObj.species.name;
+        $("#poke2pic").hide();
         document.getElementById("poke1pic").src = myObj.sprites.front_default;
         document.getElementById("pokAbility").innerHTML = myObj.abilities[0].ability.name;
-        //document.getElementById("pokeHeight").innerHTML = myObj.height
+        document.getElementById("pokeHeight").innerHTML = myObj.height;
         document.getElementById("pokeWeight").innerHTML = myObj.weight;
         document.getElementById("pokeType").innerHTML = myObj.types[0].type.name;
     }
@@ -52,16 +53,6 @@ function getPokemon(){
 
 //get intended pokemon
 function getIntendedPokemon(num){
-    // pokemonNumber = getIntendedPokemon();
-    // var str = document.getElementById("searchPokemon").value;
-    // // var pattern = /[1-9]/g;
-    // let pattern = /\b(0|[1-9][0-9]?|100)\b/g;
-    // let result = str.match(pattern);
-    // console.log(str);
-    // // console.log(pattern.test(str)+ " here");
-    // if(pattern.test(str)){
-    //     console.log(str + " here 63 ");
-        
 
        console.log("in function "+ num);
     
@@ -69,15 +60,26 @@ function getIntendedPokemon(num){
         xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
+            $("#poke2").show();
             document.getElementById("poke2").innerHTML = myObj.species.name;
             $("#poke1").hide();
 
+            $("#poke2pic").show();
             document.getElementById("poke2pic").src = myObj.sprites.front_default;
             $("#poke1pic").hide();
-            // document.getElementById("pokAbility2").innerHTML = myObj.abilities[0].ability.name;
-            // document.getElementById("pokeHeight2").innerHTML = myObj.height
-            // document.getElementById("pokeWeight2").innerHTML = myObj.weight;
-            // document.getElementById("pokeType2").innerHTML = myObj.types[0].type.name;
+            $("#pokAbility2").show();
+            document.getElementById("pokAbility2").innerHTML = myObj.abilities[0].ability.name;
+            $("#pokAbility").hide();
+            $("#pokeHeight2").show();
+            document.getElementById("pokeHeight2").innerHTML = myObj.height;
+            $("#pokeHeight").hide();
+
+            $("#pokeWeight2").show();
+            document.getElementById("pokeWeight2").innerHTML = myObj.weight;
+            $("#pokeWeight").hide();
+            $("#pokeType2").show();
+            document.getElementById("pokeType2").innerHTML = myObj.types[0].type.name;
+            $("#pokeType").hide();
             console.log(myObj.species.name);
         }
         };
@@ -92,7 +94,7 @@ function getNum(){
 function getIntendedPokemon1(){
     var str = document.getElementById("searchPokemon").value;
     // var pattern = /[1-9]/g;
-    let pattern = /\b(0|[1-9][0-9]?|100)\b/g;
+    let pattern = /\b(0|[1-9][0-9]?|150)\b/g;
     let result = str.match(pattern);
     //console.log(str);
     //console.log(pattern.test(str)+ " here");
